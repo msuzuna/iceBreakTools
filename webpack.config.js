62c0f6f1d52bc7c23ed423b5f297ff809f546931
suzunamatsuo <suzunamatsuo@waku-2.com>
2023-04-29 16:17:36 +0900

    開発環境の構築

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   mode: isDev ? 'development' : 'production',
   // mode: "development",
 
-  entry: './src/js/index.js',
+  entry: './src/ts/index.ts',
 
   output: {
     path: path.join(__dirname, '/dist'),
@@ -88,6 +88,12 @@ module.exports = {
         test: /\.pug$/,
         use: 'pug-loader',
       },
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'ts-loader',
+      },
     ],
   },
   plugins: [
@@ -103,4 +109,12 @@ module.exports = {
     }),
     new HtmlWebpackPugPlugin(),
   ],
+  // import 文で .ts ファイルを解決するため
+  // これを定義しないと import 文で拡張子を書く必要が生まれる。
+  // フロントエンドの開発では拡張子を省略することが多いので、
+  // 記載したほうがトラブルに巻き込まれにくい。
+  resolve: {
+    // 拡張子を配列で指定
+    extensions: ['.ts', '.js'],
+  },
 };
