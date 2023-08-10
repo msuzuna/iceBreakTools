6eedebeddfa87ace556e095e7bfd1d9b78980faa
suzunamatsuo <suzunamatsuo@waku-2.com>
2023-08-10 15:51:11 +0900

    相談内容

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,11 +21,14 @@ export const fetchBookInfo = functions.https.onRequest(
   }
 );
 
+// ①リクエストを受け取ったらハローワールドを返すからスタートするのが良さそう
+// 必要なデータを持ってきてくっつけて返す
+
 export const requestOpenAI = functions.https.onRequest(
   async (request, response) => {
     try {
       const configuration = new Configuration({
-        organization: `${process.env.VITE_OPENAI_ORG}`,
+        organization: `${process.env.VITE_OPENAI_ORG}`, // 環境変数の書き方を変えた方が良さそう！
         apiKey: `${process.env.VITE_OPENAI_API_KEY}`,
       });
       const openai = new OpenAIApi(configuration);
@@ -57,3 +60,14 @@ export const requestOpenAI = functions.https.onRequest(
     }
   }
 );
+
+/* トークテーマごとにパスを切る　リクエストごとにパスを変える
+ * クエリパラメータごとに変える　URL　キーとバリュー
+　　　クライアントに投げるときにリクエストにくっつけてfetchする
+　　　fetch APIを使うことになる　難しい処理になりそう
+　　　②リクエスト側でクエリを設定
+　　　③受け取ったクエリをそのまま返す
+　　　④受け取ったクエリをopenAIに投げる
+　　　⑤openAI側から受け取った内容をクライアント側に返す
+　　　サーバーからクエリを抽出してfetchして返ってきたのをクライアントに返す
+ */
