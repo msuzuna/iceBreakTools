f5becec9cbfc6b26e5b4d6b2781659d15cb50d72
msuzuna <suzuna.matsuo@nijibox.co.jp>
2023-09-18 19:40:10 +0900

    add-トークテーマ一覧画面の作成

---
+++ src/roomtest/ts/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAUEFVZy7KYZtj_vgUo1og54740oXkNMYs',
+  authDomain: 'icebreak-727fe.firebaseapp.com',
+  databaseURL:
+    'https://icebreak-727fe-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'icebreak-727fe',
+  storageBucket: 'icebreak-727fe.appspot.com',
+  messagingSenderId: '1025318167864',
+  appId: '1:1025318167864:web:c33e34d45a67054e9f4dcd',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
